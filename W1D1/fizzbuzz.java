395dca60620f7c40687d1e53140584aceb24218f
Ctwice <carpe359@gmail.com>
2015-02-24 09:47:02 -0600

    fizzbuzz

---
+++ W1D1/fizzbuzz.java
@@ -0,0 +1,22 @@
+public class fizzbuzz {
+    public static void main(string[] args) {
+    
+    for (i = 1; i <= 100; i++) {
+    
+    if (i % 3 == 0) {
+    system.out.println("fizz");
+    
+    } if (i % 5 == 0){
+    system.out.println("buzz");
+    }
+    else (i % 15 == 0) {
+    system.out.println("fizzbuzz")
+    }
+    }
+    
+    
+    }
+
+
+
+}
