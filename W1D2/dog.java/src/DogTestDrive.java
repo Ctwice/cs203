c3eeb9f3e10de59d19671976a527bbc580f6f86e
Ctwice <carpe359@gmail.com>
2015-02-24 14:42:49 -0600

    DogTestDrive.java

---
+++ W1D2/dog.java/src/DogTestDrive.java
@@ -0,0 +1,15 @@
+
+
+public class DogTestDrive {
+
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.size = 45;
+        d.breed = "labrador"; 
+        d.bark();
+        
+    }
+
+}
+
+
